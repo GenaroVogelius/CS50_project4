e9c0cc15878f88ecbc19f87dc3b0a43dac2215e6
Genaro Vogelius <genagevo@gmail.com>
2023-05-07 20:30:54 -0300

    changes

--- network/react-front/src/components/Content.jsx
+++ network/react-front/src/components/Content.jsx
@@ -20,7 +20,7 @@ function Content(props) {
 
 
     function HandleSubmit(event) {
-      event.preventDefault();
+      
       let input = event.target.editContent;
       console.log(input.value)
 
